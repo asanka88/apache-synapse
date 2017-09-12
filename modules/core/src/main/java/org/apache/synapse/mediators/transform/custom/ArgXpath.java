4965b373db4ba83622c9f595cbc5aeb9642351da
Asanka Dissanayake <asankanbd@gmail.com>
2017-09-12 00:00:33 -0400

    modifying getformattedValue to support only Strings

--- modules/core/src/main/java/org/apache/synapse/mediators/transform/custom/ArgXpath.java
+++ modules/core/src/main/java/org/apache/synapse/mediators/transform/custom/ArgXpath.java
@@ -1,16 +1,18 @@
 package org.apache.synapse.mediators.transform.custom;
 
-import org.apache.axiom.om.*;
-import org.apache.axiom.om.impl.llom.OMTextImpl;
+import org.apache.axiom.om.OMAttribute;
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.OMText;
 import org.apache.commons.lang.StringUtils;
 import org.apache.synapse.MessageContext;
+import org.apache.synapse.config.xml.enums.PropertyTypes;
 import org.apache.synapse.util.xpath.SynapseXPath;
 import org.jaxen.JaxenException;
-import org.apache.synapse.config.xml.enums.*;
 
 import java.util.ArrayList;
-import java.util.IllegalFormatException;
-import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * Created by asanka on 3/21/16.
@@ -29,33 +31,81 @@ public class ArgXpath extends SynapseXPath {
 
     public Object getFormattedResult(MessageContext messageContext) throws JaxenException {
         Object evaluate = this.evaluate(messageContext);
-        if(this.type==null){
-            return evaluate;
-        }else if(type==PropertyTypes.string){
-            if(evaluate instanceof ArrayList){
-                ArrayList tmpList=(ArrayList)evaluate;
-                int size = tmpList.size();
-                if(size >1){
-                    ArrayList<String> result=new ArrayList<String>(tmpList.size());
-                    Iterator iterator = tmpList.iterator();
-                    while (iterator.hasNext()){
-                        Object next = iterator.next();
-                        result.add(getString(next));
-                    }
-                    return result;
-                }else if(size==1){
-                    Object o = tmpList.get(0);
-                    return getString(o);
+
+        PropertyTypes argType = Optional.ofNullable(this.type).orElse(PropertyTypes.string);
+
+        //if list
+
+//        if(evaluate instanceof ArrayList){
+//            ArrayList tmpList=(ArrayList)evaluate;
+//
+//            if(tmpList.size()==1){
+//                if(this.type==null){
+//                    return tmpList.get(0);
+//                }else{
+//                   return getString(((List) evaluate).get(0));
+//                }
+//            }else if(tmpList.size()>1){
+//                ArrayList<String> result=new ArrayList<String>(tmpList.size());
+//
+//               tmpList.stream().map(s -> getString(s)).collect(Collectors.toList());
+//
+//                return result;
+//
+//            }
+//        }
+
+
+
+        if(argType==PropertyTypes.string){
+            if (evaluate instanceof ArrayList) {
+                ArrayList tmpList = (ArrayList) evaluate;
+
+                if (tmpList.size() == 1) {
+                        return getString(((List) evaluate).get(0));
+
+                } else if (tmpList.size() > 1) {
+
+                    Object collect = tmpList.stream().map(o -> getString(o)).collect(Collectors.toList());
+
+                    return collect;
+
+                }else{
+                    return "";
                 }
-
             }
-
         }
+
+
+
+
+//        if(this.type==null){
+//            return evaluate;
+//        }else if(type==PropertyTypes.string){
+//            if(evaluate instanceof ArrayList){
+//                ArrayList tmpList=(ArrayList)evaluate;
+//                int size = tmpList.size();
+//                if(size >1){
+//                    ArrayList<String> result=new ArrayList<String>(tmpList.size());
+//                    Iterator iterator = tmpList.iterator();
+//                    while (iterator.hasNext()){
+//                        Object next = iterator.next();
+//                        result.add(getString(next));
+//                    }
+//                    return result;
+//                }else if(size==1){
+//                    Object o = tmpList.get(0);
+//                    return getString(o);
+//                }
+//
+//            }
+//
+//        }
         return evaluate;
     }
     private String getString(Object o){
         if(o instanceof OMElement){
-            return ((OMElement) o).toString();
+            return o.toString();
         }else if(o instanceof OMText){
             return ((OMText) o).getText();
         }else if(o instanceof OMAttribute){
